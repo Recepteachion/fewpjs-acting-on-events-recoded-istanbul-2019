54d84626480835004ec6589b8b76912d73450eb0
Recep Öztürk <turkishteacher23@gmail.com>
2020-01-19 21:16:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+// Your code herelet dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor="#FF69B4";
+dodger.style.bottom = "0px";
+dodger.style.right = "180px";
+dodger.style.left = "180px";
+
+
+document.addEventListener("keydown",function(e){
+	if (e.key ==="ArrowLeft"){
+		moveDodgerLeft();
+	}
+
+});
+	
+
+function moveDodgerLeft(){
+	let leftNumber = dodger.style.left.replace("px","");
+	let rightNo = dodger.style.right.replace("px","");
+	let right = parseInt(rightNo,10);
+	let left = parseInt(leftNumber,10);
+
+	if ( right >0)  {
+		dodger.style.right=`${right- 10}px`
+		dodger.style.left=`${left+ 10}px`
+	}
+}
+	
